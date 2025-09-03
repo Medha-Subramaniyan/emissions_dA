bec2ac5d671a2a95c128a1dabbcf006efee7df41
Medha <medha.subrav@gmail.com>
2025-09-03 14:02:00 -0400

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE public.annual_co_emissions_by_region (
   entity                TEXT        NOT NULL,
-  code                  TEXT,                 -- OWID/ISO-style code 
-  year                  INTEGER     NOT NULL, 
-  annual_co2_emissions  BIGINT,               
+  code                  TEXT,
+  year                  INTEGER     NOT NULL,
+  annual_co2_emissions  NUMERIC,   -- changed from BIGINT → NUMERIC
   CONSTRAINT annual_co_emissions_by_region_pk PRIMARY KEY (entity, year)
 );
 
@@ -13,3 +13,8 @@ CREATE TABLE public.continents_according_to_owid (
   world_region_owid TEXT  NOT NULL,          
   CONSTRAINT continents_according_to_owid_pk PRIMARY KEY (entity)
 );
+
+/* \copy public.continents_according_to_owid (entity, code, year, world_region_owid)
+FROM '/Users/medhasubramaniyan/Desktop/data_set/emissions/continents-according-to-our-world-in-data/continents-according-to-our-world-in-data.csv' CSV HEADER; */
+
+/*\copy public.annual_co_emissions_by_region (entity, code, year, annual_co2_emissions) FROM '/Users/medhasubramaniyan/Desktop/data_set/emissions/annual-co-emissions-by-region.csv' CSV HEADER;*/ 
